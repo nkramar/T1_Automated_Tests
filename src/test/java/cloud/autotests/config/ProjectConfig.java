1dd21100797416b6632046de7041e1d7c809b189
Kramar Nadezhda <nadezhda.kramar@telekom.com>
2022-07-04 13:13:53 +0300

    refactoring done

--- src/test/java/cloud/autotests/config/ProjectConfig.java
+++ src/test/java/cloud/autotests/config/ProjectConfig.java
@@ -10,13 +10,18 @@ import org.aeonbits.owner.Config;
 })
 public interface ProjectConfig extends Config {
 
-    @DefaultValue("chrome")
-    String browser();
-    @DefaultValue("100.0")
-    String browserVersion();
-    @DefaultValue("1920x1080")
-    String browserSize();
-    String browserMobileView();
-    String remoteDriverUrl();
-    String videoStorage();
+  @DefaultValue("chrome")
+  String browser();
+
+  @DefaultValue("100.0")
+  String browserVersion();
+
+  @DefaultValue("1920x1080")
+  String browserSize();
+
+  String browserMobileView();
+
+  String remoteDriverUrl();
+
+  String videoStorage();
 }
